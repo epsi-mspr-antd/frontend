8833b2118710df14a9eb41337692d1e958103d63
Nicolas BLET <nicolas.blet@hotmail.com>
2024-03-15 15:38:27 +0100

    Page Register

--- src/Components/Login/login.component.tsx
+++ src/Components/Login/login.component.tsx
@@ -2,31 +2,48 @@ import { BentoGeneric } from '../../BentoDesign/BentoGeneric.component';
 import { Header } from '../Header/header.component';
 import './login.style.css';
 
+import { useState } from 'react';
+
 export const Login = () => {
+    const [formData, setFormData] = useState({
+        lastName: '',
+        firstName: '',
+        email: '',
+        password: '',
+        phone: ''
+    });
+
+    const handleChange = (e: { target: { name: any; value: any; }; }) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        // TODO
+    };
+
     return(
         <>
             <BentoGeneric 
                 childHeader={<Header/>}
                 childMain={
-                    <section className='flex flex-col justify-center'>
-                        <form className='h-full flex flex-col justify-center gap-8'>
-                            <label htmlFor="last_name" >Nom :</label>
-                            <input type="text" />
-                            <label htmlFor="first_name">Prénom :</label>
-                            <input type="text" />
+                    <section className='h-full flex flex-col justify-end p-8 text-black'>
+                        <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
                             <label htmlFor="email">Email :</label>
-                            <input type="email" />
+                            <input type="email" name="email" id="email" className='inputLogin' value={formData.email} onChange={handleChange} required />
                             <label htmlFor="password">Mot de passe :</label>
-                            <input type="password" name="password" id="loginInputPassword" />
-                            <label htmlFor="phone">N° de téléphone</label>
-                            <input type="tel" name="phone" id="loginInputPhone" />
+                            <input type="password" name="password" id="password" className='inputLogin' value={formData.password} onChange={handleChange} required />
+                            <button type="submit" className="mt-8" id="btn_login_submit">Se connecter</button>
                         </form>
-                </section>
+                    </section>
                 }
                 childRight={undefined}
                 isBurgerMenu={true}
                 isSVGRequired={true}
             /> 
         </>
-    )
-}
+    );
+};
--- src/Components/Register/register.component.tsx
+++ src/Components/Register/register.component.tsx
@@ -1,9 +1,62 @@
-import './register.style';
+import './register.style.css';
+import { BentoGeneric } from '../../BentoDesign/BentoGeneric.component';
+import { Header } from '../Header/header.component';
+import { useState } from 'react';
+
 
 export const Register = () => {
+    const [formData, setFormData] = useState({
+        lastName: '',
+        firstName: '',
+        email: '',
+        password: '',
+        phone: ''
+    });
+
+    const handleChange = (e: { target: { name: any; value: any; }; }) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        // TODO
+    };
+
     return(
         <>
-        
+            <BentoGeneric 
+                childHeader={<Header/>}
+                childMain={
+                    <section className='sectionRegister h-full flex flex-col justify-end p-8 text-black'>
+                        <form className='flex flex-col' onSubmit={handleSubmit}>
+                            <label htmlFor="last_name">Nom :</label>
+                            <input type="text" name="lastName" id="lastName" className='inputRegister' value={formData.lastName} onChange={handleChange} required />
+                            <label htmlFor="first_name">Prénom :</label>
+                            <input type="text" name="firstName" id="firstName" className='inputRegister' value={formData.firstName} onChange={handleChange} required />
+                            <label htmlFor="email">Email :</label>
+                            <input type="email" name="email" id="email" className='inputRegister' value={formData.email} onChange={handleChange} required />
+                            <label htmlFor="password">Mot de passe :</label>
+                            <input type="password" name="password" id="password" className='inputRegister' value={formData.password} onChange={handleChange} required />
+                            <label htmlFor="phone">N° de téléphone :</label>
+                            <input type="tel" name="phone" id="phone" className='inputRegister' value={formData.phone} onChange={handleChange} required />
+                            <button type="submit" className="btn-primary" id="btn_register_submit">S'enregister</button>
+                        </form>
+                    </section>
+                }
+                childRight={undefined}
+                isBurgerMenu={true}
+                isSVGRequired={true}
+            /> 
         </>
-    )
-}
+    );
+};
+
+// const Register = () => {
+//     const [lastName, setLastName] = useState(String)
+//         const handleChange = () => {
+//             setLastName(String),
+//             console.log(lastName)
+//         }
