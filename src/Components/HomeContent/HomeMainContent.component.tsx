9cb112dd447ac3909079e5f646724acea17c9e15
Jessee D'antonio <58628141+JesseeDantonio@users.noreply.github.com>
2024-05-24 16:47:41 +0200

    Modification des composants suivants : Login, HomeMain, Map, AccountMain. (#11)

--- src/Components/HomeContent/HomeMainContent.component.tsx
+++ src/Components/HomeContent/HomeMainContent.component.tsx
@@ -3,10 +3,10 @@ import './HomeMainContent.style.css';
 export function HomeMainContent() {
     return (
         <>
-        
+
             <section className='h-full flex flex-col justify-center gap-8 text-center'>
                 <article className='mainArticle'>
-                    <h1 className='text-left text-xl'> Nos services </h1>
+                    <h1 className='text-left text-2xl'> Nos services </h1>
                     <span className='detailsSpan text-sm text-left'>
                          <ul className='flex flex-col mt-2 gap-2 pl-2'>
                             <li> Faites garder vos plantes par un voisin </li>
@@ -15,11 +15,11 @@ export function HomeMainContent() {
                     </span>
                 </article>
                 <article className='mainArticle flex flex-col gap-3'>
-                <h1 className='text-left text-xl'> Se connecter !</h1>
-                <span className='detailsSpan text-sm text-left'> <button> Connexion </button> / <button> Inscription </button> </span>
+                    <h1 className='text-left text-2xl'> Se connecter !</h1>
+                    <span className='detailsSpan text-sm text-left'> <button> Connexion </button> / <button> Inscription </button> </span>
                 </article>
             </section>
-  
+
         </>
     )
 }
