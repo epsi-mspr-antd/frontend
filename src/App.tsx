b4429395f644bd620ac942a37c21751a18e1a090
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2024-06-01 08:52:41 +0200

    feat: add back button for android

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.css';
 
 import './../theme/button.class.css';
 
@@ -6,23 +6,42 @@ import './../theme/button.theme.css';
 import './../theme/colors.theme.css';
 import './../theme/state.theme.css';
 
-
 import { BentoGeneric } from './BentoDesign/BentoGeneric.component';
 import { HomeMainContent } from './Components/HomeContent/HomeMainContent.component';
 import { HomeRightContent } from './Components/HomeContent/HomeRightContent.component';
 import { Header } from './Components/Header/header.component';
 
+import { App as CapacitorApp } from '@capacitor/app';
+import { useEffect } from 'react';
+
 function App() {
+  useEffect(() => {
+    const handleBackButton = (_: any) => {
+      if (window.history.length > 1) {
+        window.history.back();
+      } else {
+        CapacitorApp.exitApp();
+      }
+    };
+
+    CapacitorApp.addListener('backButton', handleBackButton);
+
+    return () => {
+      CapacitorApp.removeAllListeners();
+    };
+  }, []);
+
   return (
     <>
-        <BentoGeneric 
-        childHeader={<Header/>} 
-        childMain={<HomeMainContent/>} 
-        childRight={<HomeRightContent/>} 
+      <BentoGeneric
+        childHeader={<Header />}
+        childMain={<HomeMainContent />}
+        childRight={<HomeRightContent />}
         isBurgerMenu={true}
-        isSVGRequired={true} />
+        isSVGRequired={true}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
