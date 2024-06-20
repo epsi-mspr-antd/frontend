4c7833b38dec6786a556d2de98946c181f3dcbad
N-BLET <bletou@hotmail.fr>
2024-06-20 16:16:40 +0200

    update design settingsProfil, properties & burgerMenu

--- src/Components/BurgerMenu/NavigationMenu/NavigationMenu.component.tsx
+++ src/Components/BurgerMenu/NavigationMenu/NavigationMenu.component.tsx
@@ -6,38 +6,45 @@ import { logOut } from "../../../utils/API/Auth/auth.service";
 import { removeFromLocalStorage } from "../../../utils/localStorage/localStorage.service";
 
 type Props = {
-    isOpened: boolean,
-    setIsOpened: React.Dispatch<React.SetStateAction<boolean>>
-}
+  isOpened: boolean;
+  setIsOpened: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
-export const NavigationMenu:React.FC<Props> = ({isOpened, setIsOpened}) => {
-    const authContext = useContext(AuthContext);
-    const handleClick = () => {
-        setIsOpened(!isOpened)
-    }
-    const handleLogout = () => {
-        logOut();
-        authContext.updateAccessToken('');
-        authContext.updateEmail('');
-        authContext.updateRefreshToken('');
-        authContext.updateUserID(0)
-        removeFromLocalStorage('authContext');
-    }
-    return (
-       
-            <nav className="navMenu absolute z-2 right-0 bottom-0 h-full w-[60%] flex flex-col justify-center text-center pl-8 py-6">
-                {!authContext.accessToken ?
-                    <ul className='flex flex-col h-full justify-around [&>*]:w-full [&>*]:py-4'>
-                        Veuillez vous connecter, tricheur ! 
-                    </ul>
-                    :
-                    <ul className='flex flex-col h-full justify-around [&>*]:w-full [&>*]:h-16'>
-                        <Link to={'/'}><li> Accueil </li></Link>
-                        <Link to={'/account'} onClick={() => handleClick()}><li> Mon compte </li></Link>
-                        <Link to={'/map'} onClick={() => handleClick()}><li> Ma carte </li></Link>
-                        <Link to={'/register'} onClick={() => handleLogout()}><li> Se déconnecter </li></Link>
-                    </ul>
-                }
-                </nav>
-                )
+export const NavigationMenu: React.FC<Props> = ({ isOpened, setIsOpened }) => {
+  const authContext = useContext(AuthContext);
+  const handleClick = () => {
+    setIsOpened(!isOpened);
+  };
+  const handleLogout = () => {
+    logOut();
+    authContext.updateAccessToken("");
+    authContext.updateEmail("");
+    authContext.updateRefreshToken("");
+    authContext.updateUserID(0);
+    removeFromLocalStorage("authContext");
+  };
+  return (
+    <nav className="navMenu absolute z-2 right-0 bottom-0 h-full w-[60%] flex flex-col justify-center text-center pl-8 py-6">
+      {!authContext.accessToken ? (
+        <ul className="flex flex-col h-full justify-around [&>*]:w-full [&>*]:py-4">
+          Veuillez vous connecter, tricheur !
+        </ul>
+      ) : (
+        <ul className="flex flex-col h-full justify-around [&>*]:w-full [&>*]:h-16">
+          <Link to={"/"} onClick={() => handleClick()}>
+            <li> Accueil </li>
+          </Link>
+          <Link to={"/account"} onClick={() => handleClick()}>
+            <li> Mon compte </li>
+          </Link>
+          <Link to={"/map"} onClick={() => handleClick()}>
+            <li> Ma carte </li>
+          </Link>
+          <Link to={"/login"} onClick={() => handleLogout()}>
+            <li> Se déconnecter </li>
+          </Link>
+        </ul>
+      )}
+    </nav>
+  );
 };
